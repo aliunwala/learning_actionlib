a0ebe3c77f3b414670d7f8152ed4c60e7ec288b2
goatfigpc <goatfigpc@goatfigpc-ThinkPad-T430s.(none)>
2014-04-09 02:56:05 -0500

    moving to real robot

--- src/move_robot.cpp
+++ src/move_robot.cpp
@@ -9,35 +9,35 @@
 int main(int argc, char ** argv){
     const float a = sqrt(2);
     geometry_msgs::PoseStamped outsideElevPose;
-    outsideElevPose.header.seq = 4; // Arbitriray?
-    outsideElevPose.header.stamp.sec= 491;// Arbitriray?
+    outsideElevPose.header.seq = 0; // Arbitriray?
+    outsideElevPose.header.stamp.sec= 0;// Arbitriray?
     outsideElevPose.header.frame_id = "map"; 
-    outsideElevPose.pose.position.x = 24;
-    outsideElevPose.pose.position.y = 11;
+    outsideElevPose.pose.position.x = 10.3;
+    outsideElevPose.pose.position.y = 50.12;
     outsideElevPose.pose.position.z = 0;
     outsideElevPose.pose.orientation.x = 0;
     outsideElevPose.pose.orientation.y = 0;
-    outsideElevPose.pose.orientation.z = -a;
-    outsideElevPose.pose.orientation.w = a;
+    outsideElevPose.pose.orientation.z = 1;
+    outsideElevPose.pose.orientation.w = 0;
 
     geometry_msgs::PoseStamped insideElevPose;
-    insideElevPose.header.seq = 4;
-    insideElevPose.header.stamp.sec= 492;
+    insideElevPose.header.seq = 0;
+    insideElevPose.header.stamp.sec= 0;
     insideElevPose.header.frame_id = "map"; 
-    insideElevPose.pose.position.x = 24;
-    insideElevPose.pose.position.y = 8;
+    insideElevPose.pose.position.x = 6.97;
+    insideElevPose.pose.position.y = 50.42;
     insideElevPose.pose.position.z = 0;
     insideElevPose.pose.orientation.x = 0;
     insideElevPose.pose.orientation.y = 0;
-    insideElevPose.pose.orientation.z = a;
-    insideElevPose.pose.orientation.w = a;
+    insideElevPose.pose.orientation.z = 0;
+    insideElevPose.pose.orientation.w = 1;
 
     geometry_msgs::PoseStamped outsideElevPoseFloor2;
-    outsideElevPoseFloor2.header.seq = 4; // Arbitriray?
-    outsideElevPoseFloor2.header.stamp.sec= 491;// Arbitriray?
+    outsideElevPoseFloor2.header.seq = 0; // Arbitriray?
+    outsideElevPoseFloor2.header.stamp.sec= 0;// Arbitriray?
     outsideElevPoseFloor2.header.frame_id = "map"; 
-    outsideElevPoseFloor2.pose.position.x = 24;
-    outsideElevPoseFloor2.pose.position.y = 11;
+    outsideElevPoseFloor2.pose.position.x = 24.17;
+    outsideElevPoseFloor2.pose.position.y = 32.3;
     outsideElevPoseFloor2.pose.position.z = 0;
     outsideElevPoseFloor2.pose.orientation.x = 0;
     outsideElevPoseFloor2.pose.orientation.y = 0;
@@ -49,8 +49,8 @@ int main(int argc, char ** argv){
     initalPositionFloor2.header.stamp.sec = 0;
     initalPositionFloor2.header.stamp.nsec = 0;
     initalPositionFloor2.header.frame_id = "map";
-    initalPositionFloor2.pose.pose.position.x = 24.84  ;
-    initalPositionFloor2.pose.pose.position.y = 7.86;
+    initalPositionFloor2.pose.pose.position.x = 24.46  ;
+    initalPositionFloor2.pose.pose.position.y = 29.62;
     initalPositionFloor2.pose.pose.position.z = 0 ;
     initalPositionFloor2.pose.pose.orientation.x = 0;
     initalPositionFloor2.pose.pose.orientation.y = 0;
@@ -63,27 +63,27 @@ int main(int argc, char ** argv){
 
     ros::init( argc, argv, "move_robot");
     ros::NodeHandle n;
-    ros::ServiceClient client_opendoors = n.serviceClient<segbot_simulation_apps::DoorHandlerInterface>("update_doors");
-    segbot_simulation_apps::DoorHandlerInterface open_door;
-    open_door.request.door = "" ;
-    open_door.request.open = true;
-    open_door.request.all_doors= true;
-    if (client_opendoors.call(open_door)){
-        ROS_INFO("running the service");
-    }
-    else{
-        ROS_ERROR("Failed the service");
-    }
+    //ros::ServiceClient client_opendoors = n.serviceClient<segbot_simulation_apps::DoorHandlerInterface>("update_doors");
+    //segbot_simulation_apps::DoorHandlerInterface open_door;
+    //open_door.request.door = "" ;
+    //open_door.request.open = true;
+    //open_door.request.all_doors= true;
+    //if (client_opendoors.call(open_door)){
+        //ROS_INFO("running the service");
+    //}
+    //else{
+        //ROS_ERROR("Failed the service");
+    //}
 
-
+    ros::ServiceClient client = n.serviceClient<map_mux::ChangeMap>("change_map");
+    map_mux::ChangeMap srv ;
 
 
     boost::shared_ptr<actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> > robot_controller_;
     ros::Publisher initialPosePub= n.advertise<geometry_msgs::PoseWithCovarianceStamped>("initialpose", 1000);
 
     ROS_INFO("wating");
-    robot_controller_.reset(
-    new actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>("move_base", true));
+    robot_controller_.reset( new actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction>("move_base", true));
     robot_controller_->waitForServer();
 
 
@@ -92,14 +92,12 @@ int main(int argc, char ** argv){
    goal.target_pose = outsideElevPose;
    robot_controller_->sendGoal(goal);
    robot_controller_->waitForResult();
-   
    goal.target_pose = insideElevPose;
+   
    robot_controller_->sendGoal(goal);
    robot_controller_->waitForResult();
 // 1) Wait for user input as to what floor I am on
 // 2) switch map
-    ros::ServiceClient client = n.serviceClient<map_mux::ChangeMap>("change_map");
-    map_mux::ChangeMap srv ;
     srv.request.data = 2;
     if (client.call(srv)){
     }
